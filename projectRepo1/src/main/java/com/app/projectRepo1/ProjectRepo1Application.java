f49b48c74cfe7679a43230eb34b1ea7a3b97812b
Windows10 <Windows10@DESKTOP-4BLCQKE>
2022-10-12 11:30:27 +0700

    Commit First

---
+++ projectRepo1/src/main/java/com/app/projectRepo1/ProjectRepo1Application.java
@@ -0,0 +1,13 @@
+package com.app.projectRepo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectRepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectRepo1Application.class, args);
+	}
+
+}
---
+++ projectRepo1/src/test/java/com/app/projectRepo1/ProjectRepo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.projectRepo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectRepo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
